2735cae2ec424e1a02545f5d989ee11c74230256
bertdd <bertdd@me.com>
2023-06-15 14:48:45 +0200

    made cells dynamic

--- Memories/MainPage.xaml.cs
+++ Memories/MainPage.xaml.cs
@@ -5,6 +5,16 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+
+		for (int i = 0; i < 4; i++)
+		{
+			grid.RowDefinitions.Add(new RowDefinition() { Height = BlockSize });
+		}
+
+		for (int i = 0; i < 6; i++)
+		{
+			grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = BlockSize });
+		}
 	}
 
   private void ImageButton_Clicked(object sender, EventArgs e)
@@ -13,5 +23,7 @@ public partial class MainPage : ContentPage
 		image.RotateYTo(180);
 		image.FadeTo(0, 500);
   }
+
+	const int BlockSize = 100;
 }
 
