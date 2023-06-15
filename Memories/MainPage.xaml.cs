0d67f1b7f6db82915cb9f245e705c3abdd7cbaa6
bertdd <bertdd@me.com>
2023-06-15 15:19:50 +0200

    added the board

--- Memories/MainPage.xaml.cs
+++ Memories/MainPage.xaml.cs
@@ -6,16 +6,31 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 
-		for (int i = 0; i < 4; i++)
+		for (int i = 0; i < Rows; i++)
 		{
 			grid.RowDefinitions.Add(new RowDefinition() { Height = BlockSize });
 		}
 
-		for (int i = 0; i < 6; i++)
+		for (int i = 0; i < Columns; i++)
 		{
 			grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = BlockSize });
 		}
-	}
+
+		foreach (var tile in tiles)
+		{
+			grid.Children.Add(tile);
+			tile.Clicked += ImageButton_Clicked;
+		}
+  }
+
+  readonly Tile[] tiles = new Tile[]
+	{
+    new Tile("lionel.png", 0, 0, new Color(255, 255, 0)),
+    new Tile("don.png", 3, 5, new Color(0, 255, 0)),
+		new Tile("milan.png", 3, 4, new Color(0, 255, 255))
+	};
+
+  readonly Tile[,] board = new Tile[Rows, Columns];
 
   private void ImageButton_Clicked(object sender, EventArgs e)
   {
@@ -25,5 +40,7 @@ public partial class MainPage : ContentPage
   }
 
 	const int BlockSize = 100;
+	const int Rows = 4;
+	const int Columns = 6;
 }
 
---
+++ Memories/Tile.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Memories;
+
+public class Tile : ImageButton
+{
+  public Tile(string source, int row, int col, Color color)
+  {
+    Source = source;
+    BackgroundColor = color;
+    SetValue(Grid.RowProperty, row);
+    SetValue(Grid.ColumnProperty, col);
+  }
+}
